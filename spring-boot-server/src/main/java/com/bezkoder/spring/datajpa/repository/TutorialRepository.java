68e8294be4da8a4c04b7b722b9fc48bde05c6c53
camelliabi <camelliabi0316@gmail.com>
2026-02-04 13:45:03 +1300

    Add custom query methods for advanced filtering

--- spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/repository/TutorialRepository.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/repository/TutorialRepository.java
@@ -3,10 +3,17 @@ package com.bezkoder.spring.datajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bezkoder.spring.datajpa.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 	List<Tutorial> findByPublished(boolean published);
 	List<Tutorial> findByTitleContaining(String title);
+	
+	// BUG #18: Custom query with incorrect JPQL - using wrong operator
+	// This will cause runtime errors when executed
+	@Query("SELECT t FROM Tutorial t WHERE t.title = :title")
+	List<Tutorial> findByTitleContaining(@Param("title") String title, @Param("published") boolean published);
 }
